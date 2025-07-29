9b3d2bf114f03d1a57ab131cb35da45ac0a70574
Braulio Rivas <brauliorivas2004@gmail.com>
2025-07-29 09:26:44 -0500

    clean wiki text and upload function

--- clean.py
+++ clean.py
@@ -9,7 +9,10 @@ MAX = 0xffffffffffffffff
 
 def clean_file(files: list[str], records: int, process_file: Callable[[str, TextIO, int], None], verbose: bool, permuted: bool, sorted_trace: bool) -> None:
     for original_file_name in files:
-        new_file_name = f"{original_file_name[:-4]}_{'full' if records == -1 else records}_clean"
+        new_file_name = original_file_name
+        if "." in original_file_name:
+            new_file_name = original_file_name[:-4]
+        new_file_name = f"{new_file_name}_{'full' if records == -1 else records}_clean"
         if permuted:
             new_file_name = f"{new_file_name}_permuted"
         new_file_name = f"{new_file_name}.{FT}"
@@ -17,22 +20,30 @@ def clean_file(files: list[str], records: int, process_file: Callable[[str, Text
             process_file(original_file_name, new_file, records, permuted, sorted_trace)
 
 
-def ibm_object_store(original_file: str, new_file: TextIO, records: int, permuted: bool) -> None:
+def ibm_object_store(original_file: str, new_file: TextIO, records: int, permuted: bool, sorted_trace: bool) -> None:
     i = 0
-    with open(original_file, "r") as file:
-        for line in file:
-            splited_line = line.split(" ")
-            time_stamp, request_type, id = splited_line[:3]
-            if request_type != "REST.GET.OBJECT":
-                continue
-            id = f"id-{id}"
-            if len(splited_line) >= 4:
-                new_file.write(f"{id} {splited_line[3]}\n")
-            else:
-                new_file.write(f"{id}\n")
-            i += 1
-            if i == records:
-                break
+    file = open(original_file, "r")
+    new_file.write("# time, object, size")
+    old_time_stamp = 0
+
+    for line in file:
+        splited_line = line.strip().split(" ")
+        time_stamp, request_type, id = splited_line[:3]
+        if request_type != "REST.GET.OBJECT":
+            continue
+        if len(splited_line) < 4:
+            continue
+        object_size = splited_line[3]
+        time_stamp = int(time_stamp)
+        if time_stamp < old_time_stamp and not sorted_trace:
+            continue
+        old_time_stamp = time_stamp
+        id_hash = mmh3.hash64(id)[0] & MAX
+        object_size = int(object_size)
+        new_file.write(f"\n{time_stamp}, {id_hash}, {object_size}")
+        i += 1
+        if i == records:
+            break
 
 
 def memcached_twitter(original_file: str, new_file: TextIO, records: int, permuted: bool, sorted_trace: bool) -> None:
@@ -58,13 +69,55 @@ def memcached_twitter(original_file: str, new_file: TextIO, records: int, permut
     file.close()
 
 
-def wiki(original_file: str, new_file: TextIO, records: int, permuted: bool) -> None:
-    pass
+def wiki_upload(original_file: str, new_file: TextIO, records: int, permuted: bool, sorted_trace: bool) -> None:
+    i = 0
+    new_file.write("# time, object, size")
+    old_time_stamp = 0
+    file = open(original_file, "r")
+    file.readline()
+    for line in file:
+        splited_line = line.strip().split()
+        time_stamp, id, image_type, value_size, _ = splited_line
+        time_stamp = int(time_stamp)
+        if time_stamp < old_time_stamp and not sorted_trace:
+            continue
+        old_time_stamp = time_stamp
+        id_hash = mmh3.hash64(id)[0] & MAX
+        value_size = int(value_size)
+        new_file.write(f"\n{time_stamp}, {id_hash}, {value_size}")
+        i += 1
+        if i == records:
+            break
+    file.close()
+
+
+def wiki_text(original_file: str, new_file: TextIO, records: int, permuted: bool, sorted_trace: bool) -> None:
+    i = 0
+    new_file.write("# time, object, size")
+    old_time_stamp = 0
+    file = open(original_file, "r")
+    file.readline()
+    for line in file:
+        splited_line = line.strip().split()
+        time_stamp, id, value_size, _ = splited_line
+        time_stamp = int(time_stamp)
+        if time_stamp < old_time_stamp and not sorted_trace:
+            continue
+        old_time_stamp = time_stamp
+        id_hash = mmh3.hash64(id)[0] & MAX
+        value_size = int(value_size)
+        new_file.write(f"\n{time_stamp}, {id_hash}, {value_size}")
+        i += 1
+        if i == records:
+            break
+    file.close()
 
 
 trace_algorithms = {
     "ibm_object_store": ibm_object_store,
     "memcached_twitter": memcached_twitter,
+    "wiki_upload": wiki_upload,
+    "wiki_text": wiki_text,
 }
 
 
