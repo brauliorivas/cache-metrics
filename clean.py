9bb8b823c18e2c0a23f2c0a3c7c6802dbe29617c
Braulio Rivas <brauliorivas2004@gmail.com>
2025-09-11 16:39:43 -0500

    take smallest size on repeated elements, measure collisions

--- clean.py
+++ clean.py
@@ -3,6 +3,7 @@ import getopt
 import mmh3
 import random
 import os
+from collections import defaultdict, Counter
 
 FT = "csv"
 MAX = 0xffffffffffffffff
@@ -16,9 +17,22 @@ class Cleaner:
         self.shuffle = shuffle
         self.old_time_stamp = 0
         self.verbose = verbose
+        self.hashes = defaultdict(set)
 
     def hash_id(self, id: str):
         return mmh3.hash64(id)[0] & MAX
+
+    def filter_file_lines(self, line):
+        new_line = self.process_line(line.strip())
+        if new_line is None:
+            return False
+        time_stamp, id, object_size = new_line
+        if time_stamp < self.old_time_stamp:
+            self.j += 1
+            return False
+        else:
+            self.old_time_stamp = time_stamp
+        return True
 
     def clean_file(self, original_file_path: str, remove_ext: bool):
         if remove_ext:
@@ -37,55 +51,76 @@ class Cleaner:
 
         new_file = open(new_file_path, "w")
         new_file.write("# time, object, size")
-
         old_file = open(original_file_path, "r")
+
         if isinstance(self, WikiText) or isinstance(self, WikiUpload) or isinstance(self, MetaCDN):
             old_file.readline()
 
+        i = 0
+        j = 0
         k = 0
-        if self.shuffle:
-            i = 0
-            lines = []
-            for line in old_file:
-                k += 1
-                new_line = self.process_line(line.strip())
-                if new_line is None:
-                    continue
-                _, _, object_size = new_line
-                if object_size == 0:
-                    continue
-                lines.append(new_line)
-                i += 1
-                if i == self.records:
-                    break
-            random.shuffle(lines)
-            for line in lines:
-                _, id, object_size = line
-                new_file.write(f"\n0, {id}, {object_size}")
-        else:
-            i = 0
-            j = 0
-            for line in old_file:
-                k += 1
-                new_line = self.process_line(line.strip())
-                if new_line is None:
-                    continue
-                time_stamp, id, object_size = new_line
-                if object_size == 0:
-                    continue
-                if not self.sorted_trace and time_stamp < self.old_time_stamp:
-                    j += 1
-                    continue
+        trace = []
+        sizes = {}
+        ignored_objects = set()
+        count = Counter()
+
+        while i != self.records:
+            line = old_file.readline()
+            k += 1
+            new_line = self.process_line(line.strip())
+            if new_line is None:
+                continue
+            time_stamp, id, object_size = new_line
+            if id in ignored_objects:
+                continue
+            if time_stamp < self.old_time_stamp:
+                j += 1
+                continue
+            else:
                 self.old_time_stamp = time_stamp
-                new_file.write(f"\n0, {id}, {object_size}")
+            if id in sizes:
+                sizes[id] = min(object_size, sizes[id])
+            else:
+                sizes[id] = object_size
+            if sizes[id] <= 0:
+                ignored_objects.add(id)
+                i -= count[id]
+            else:
+                trace.append(id)
+                count[id] += 1
                 i += 1
-                if i == self.records:
-                    break
-            print(f"There are {j} requests not sorted")
+
+        print(f"There are {j} requests not sorted")
         print(f"First {k} lines were read")
+
+        assert sum(count.values()) == i
+
+        trace = [obj for obj in trace if obj not in ignored_objects]
+
+        assert len(trace) == i
+
+        if self.shuffle:
+            random.shuffle(trace)
+
+        for obj in trace:
+            new_file.write(f"\n0, {obj}, {sizes[obj]}")
+
+        print(f"There are {len(self.hashes)} unique elements")
+        max_collisions = 0
+        most_crashed = ""
+        for k, v in self.hashes.items():
+            if len(v) > max_collisions:
+                max_collisions = len(v)
+                most_crashed = k
+        print(f"Id {most_crashed} crashed with {max_collisions} ids")
         new_file.close()
         old_file.close()
         os.rename(new_file_path, "_".join([prefix, str(i), suffix]))
+
+    def add_hashed_id(self, original_id):
+        hashed_id = self.hash_id(original_id)
+        self.hashes[hashed_id].add(original_id)
+        return hashed_id
 
 
 class IBMObjectStore(Cleaner):
@@ -100,7 +135,7 @@ class IBMObjectStore(Cleaner):
             return
         object_size = splited_line[3]
         time_stamp = int(time_stamp)
-        return time_stamp, self.hash_id(id), int(object_size)
+        return time_stamp, self.add_hashed_id(id), int(object_size)
 
 
 class MemcachedTwitter(Cleaner):
@@ -112,7 +147,7 @@ class MemcachedTwitter(Cleaner):
         if operation != "get":
             return
         time_stamp = int(time_stamp)
-        return time_stamp, self.hash_id(id), int(object_size)
+        return time_stamp, self.add_hashed_id(id), int(object_size)
 
 
 class WikiUpload(Cleaner):
@@ -120,7 +155,7 @@ class WikiUpload(Cleaner):
         splited_line = line.split()
         time_stamp, id, _, object_size, _ = splited_line
         time_stamp = int(time_stamp)
-        return time_stamp, self.hash_id(id), int(object_size)
+        return time_stamp, self.add_hashed_id(id), int(object_size)
 
 
 class WikiText(Cleaner):
@@ -128,7 +163,7 @@ class WikiText(Cleaner):
         splited_line = line.split()
         time_stamp, id, object_size, _ = splited_line
         time_stamp = int(time_stamp)
-        return time_stamp, self.hash_id(id), int(object_size)
+        return time_stamp, self.add_hashed_id(id), int(object_size)
 
 
 class MetaCDN(Cleaner):
@@ -138,7 +173,9 @@ class MetaCDN(Cleaner):
         splited_line = line.split(MetaCDN.sep)
         time_stamp, id, object_size, _ = splited_line
         time_stamp = int(time_stamp)
-        return time_stamp, self.hash_id(id), int(object_size)
+        id_hash = self.add_hashed_id(id)
+        self.hashes[id_hash].add(id)
+        return time_stamp, self.add_hashed_id(id), int(object_size)
 
 
 traces = {
